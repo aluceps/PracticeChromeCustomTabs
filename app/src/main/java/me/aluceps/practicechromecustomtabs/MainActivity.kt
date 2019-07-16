cd9e09460226e14d3d8f5a2cb06d84bf5223008e
Atsushi Takano <aluceps@gmail.com>
2019-07-16 23:26:23 +0900

    Enable databinding

--- app/src/main/java/me/aluceps/practicechromecustomtabs/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicechromecustomtabs/MainActivity.kt
@@ -1,12 +1,18 @@
 package me.aluceps.practicechromecustomtabs
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import me.aluceps.practicechromecustomtabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
